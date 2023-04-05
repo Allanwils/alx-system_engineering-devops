74959019a4839b44e038064baf4c922ad0f3333c
Allan Masiga <zionalehebrew@gmail.com>
2023-04-05 06:40:09 -0700

    Hardcode

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,46 +1,67 @@
 #!/usr/bin/env bash
 
-# Install Nginx
-class { 'nginx':
-  manage_repo => true,
+# Adds a  stable version of nginx
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# Define Nginx virtual host
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => "server {
+# updates  software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# installs nginx
+package { 'nginx':
+  ensure     => 'installed',
+}
+
+# creates a  custom header configuration file
+file { '/etc/nginx/conf.d/custom_header.conf':
+  ensure  => file,
+  content => "add_header X-Served-By 102967-web-02,
+}
+
+# removes default nginx configuration
+file { '/etc/nginx/sites-enabled/default':
+  ensure => absent,
+}
+
+# adds a  redirection and error page
+file { 'Nginx custom config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/custom_config',
+  content =>
+"server {
     listen 80 default_server;
     listen [::]:80 default_server;
     root /var/www/html;
     index index.html index.htm index.nginx-debian.html;
     server_name _;
-    add_header X-Served-By \"$hostname\";
+
+    # Includes a  custom header configuration file
+    include /etc/nginx/conf.d/custom_header.conf;
+
     location / {
-      try_files \$uri \$uri/ =404;
+        try_files \$uri \$uri/ =404;
     }
-    if (\$request_filename ~ redirect_me){
-      rewrite ^ https://youtube.com permanent;
-    }
+
     error_page 404 /404.html;
     location = /404.html {
-      internal;
+        internal;
     }
-  }",
+}",
 }
 
-# Enable Nginx virtual host
-file { '/etc/nginx/sites-enabled/default':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/default',
-  notify => Service['nginx'],
+# restart nginx
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 
-# Create default index and 404 pages
-file { '/var/www/html/index.html':
-  ensure  => 'file',
-  content => 'Hello World!',
-}
-file { '/var/www/html/404.html':
-  ensure  => 'file',
-  content => 'Ceci n\'est pas une page',
+# start service nginx
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
