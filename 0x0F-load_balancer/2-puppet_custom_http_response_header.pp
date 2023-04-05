32df3dda40d2a4df48caf23638cfc2bef7330186
Allan Masiga <zionalehebrew@gmail.com>
2023-04-05 03:41:46 -0700

    load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,86 +1,77 @@
-# The scripts add a stable version of nginx
+# The script adds a stable version of nginx
 exec { 'add nginx stable repo':
   command => 'sudo add-apt-repository ppa:nginx/stable',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# The script updates software packages list
+# Updates software packages list
 exec { 'update packages':
   command => 'apt-get update',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  require => Exec['add nginx stable repo'],
 }
 
-# installs nginx
+# Installs nginx
 package { 'nginx':
   ensure     => 'installed',
-  require    => Exec['update packages'],
 }
 
-# allows HTTP
+# Allows HTTP
 exec { 'allow HTTP':
   command => "ufw allow 'Nginx HTTP'",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
-  require => Package['nginx'],
 }
 
-# changes folder rights
+# Changes folder permissions
 exec { 'chmod www folder':
   command => 'chmod -R 755 /var/www',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# creates an index file
+# Creates a  index file
 file { '/var/www/html/index.html':
   content => "Hello World!\n",
-  require => Exec['chmod www folder'],
 }
 
-# creates a  404 error page
+# Creates a  404 file
 file { '/var/www/html/404.html':
   content => "Ceci n'est pas une page\n",
-  require => Exec['chmod www folder'],
 }
 
-# adds redirection and error page
+# Adds a custom HTTP header and error page
 file { 'Nginx default config file':
   ensure  => file,
   path    => '/etc/nginx/sites-enabled/default',
-  content =>
-"server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        add_header X-Served-By $hostname;
-
-        location / {
-            try_files \$uri \$uri/ =404;
-        }
-        
-        location = /404.html {
-            internal;
-        }
-        
-        if (\$request_filename ~ redirect_me){
-            rewrite ^ https://www.youtube.com/watch?v=msSc7Mv0QHY permanent;
-        }
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+    add_header X-Served-By \$hostname;
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+    error_page 404 /404.html;
+    location = /404.html {
+        internal;
+    }
+    if (\$request_filename ~ redirect_me){
+        rewrite ^ https://youtube.com permanent;
+    }
 }
 ",
-  require => [File['/var/www/html/index.html'], File['/var/www/html/404.html']],
 }
 
-# restarts nginx
+# Restarts nginx service
 exec { 'restart service':
   command => 'service nginx restart',
   path    => '/usr/bin:/usr/sbin:/bin',
-  require => Package['nginx'],
 }
 
-# starts service nginx
+# Starts nginx service
 service { 'nginx':
   ensure  => running,
-  require => Exec['restart service'],
+  require => Package['nginx'],
 }
