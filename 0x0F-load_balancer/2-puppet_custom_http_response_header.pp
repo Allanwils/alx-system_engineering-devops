55399068c86b42a45c3ed353c4c210c6949762c4
Allan Masiga <zionalehebrew@gmail.com>
2023-04-06 12:01:25 -0700

    customed

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,9 @@
-# Automates creation of  a custom HTTP header response, but with Puppet.
+# The script automates creation of  a custom HTTP header response, but with Puppet.
 
-file { '/path/to/script.sh':
-  mode => '0755',
-  content => "#!/bin/bash\n\napt-get -y update\napt-get -y install nginx\nsudo sed -i \"/listen 80 default_server;/a add_header X-Served-By \$HOSTNAME;\" /etc/nginx/sites-available/default\nservice nginx restart\n",
-}
-
-exec { 'install_nginx':
-  command => '/path/to/script.sh',
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
   provider => shell,
-  require => File['/path/to/script.sh'],
 }
