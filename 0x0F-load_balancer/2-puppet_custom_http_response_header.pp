201815f81524a2a2b22e882ce8efb92889299ce8
Allan Masiga <zionalehebrew@gmail.com>
2023-04-06 11:46:05 -0700

    tasked

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,14 +1,10 @@
-class nginx_custom_header {
-  $server_name = '102967-web-02'
+#!/bin/env bash
 
-  file { '/etc/nginx/conf.d/custom_headers.conf':
-    ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "add_header X-Served-By ${server_name};\n",
-    notify  => Service['nginx'],
-  }
-}
+# Automates a custom HTTP header response but with Puppet
+apt-get -y update
+apt-get -y install nginx
 
-include nginx_custom_header
+sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+
+# Restarts nginx service
+service nginx restart
