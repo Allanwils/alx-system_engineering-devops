52eeaf913f1ce2288d3c54ae78a824e8d9e3a7f4
Allan Masiga <zionalehebrew@gmail.com>
2023-05-09 08:48:54 +0300

    queries Reddit API, prints top subreddit 10 posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Function for top_ten posts listed for subreddit
+"""
+
+import requests
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        for post in response.json()["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print(None)
