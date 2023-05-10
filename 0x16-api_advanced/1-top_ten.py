9dc1743b49686df03b173e90c2df6d987d55995e
Allan Masiga <zionalehebrew@gmail.com>
2023-05-10 18:31:42 +0300

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,6 +6,16 @@ Function for top_ten posts listed for subreddit
 import requests
 
 def top_ten(subreddit):
+    """
+    Returns the top ten posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Python/requests:APIproject:v1.0.0 (by /u/Allanwils)"}
     response = requests.get(url, headers=headers)
