f8be6b589098684e0039fd78e467e90a763b7e80
Allan Masiga <zionalehebrew@gmail.com>
2023-05-11 07:38:59 +0300

    redoing task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,45 @@
 #!/usr/bin/python3
+
 """
-Function for top_ten posts listed for subreddit
+This module contains the function top_ten, which can be used to print the titles
+of the top ten hot posts for a given subreddit.
+
 """
 
 import requests
 
+
 def top_ten(subreddit):
     """
-    Returns the top ten posts for a given subreddit.
+    This function prints the titles of the top ten hot posts for a given subreddit.
 
     Args:
-        subreddit (str): The name of the subreddit.
+        subreddit (str): A string representing the subreddit whose top ten hot posts
+        need to be printed.
 
     Returns:
-        None
-
+        None: Prints the titles of the top ten hot posts for the given subreddit, or
+        None if the subreddit is invalid or there are no hot posts available.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Python/requests:APIproject:v1.0.0 (by /u/Allanwils)"}
-    response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        for post in response.json()["data"]["children"]:
-            print(post["data"]["title"])
-    else:
+    # Check if subreddit is a valid string
+    if subreddit is None or not isinstance(subreddit, str):
         print(None)
+        return
+
+    # Make a request to get the top ten hot posts for the given subreddit
+    r = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:v1.0.0 (by /u/Masiga)'},
+                     params={'limit': 10}).json()
+
+    # Check if the request was successful and get the list of posts
+    posts = r.get('data', {}).get('children', None)
+
+    # Check if there are no hot posts available for the given subreddit
+    if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
+        print(None)
+        return
+
+    # Print the titles of the top ten hot posts for the given subreddit
+    for post in posts:
+        print(post.get('data', {}).get('title', None))
