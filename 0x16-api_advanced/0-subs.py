f0a34e1a90b5cfac74ba3de1188a4a6a5011148a
Allan Masiga <zionalehebrew@gmail.com>
2023-05-11 07:25:03 +0300

    redoing task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-"""
-This module provides a function for retrieving the number of subscribers for a given subreddit on Reddit.
 
-Function:
-number_of_subscribers(subreddit: str) -> int
-- Takes a subreddit name as a string parameter.
-- Returns the number of subscribers for the given subreddit as an integer.
-- If the subreddit does not exist or an error occurs, returns 0.
+"""
+Contains function for the number_of_subscribers
 """
 
 import requests
@@ -14,37 +9,21 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Retrieve the number of subscribers for a given subreddit on Reddit.
+    This function takes a string argument representing a subreddit, and returns the number of subscribers for that subreddit.
 
     Args:
-        subreddit (str): The name of the subreddit to retrieve the number of subscribers for.
+        subreddit (str): The name of the subreddit for which to get the number of subscribers.
 
     Returns:
         int: The number of subscribers for the given subreddit.
 
-    Raises:
-        requests.exceptions.HTTPError: If a non-404 HTTP error occurs.
-        requests.exceptions.RequestException: If an error occurs while making the request.
-
+        If the subreddit is None or not a string, the function returns 0.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Python/requests:APIproject:v1.0.0 (by /u/Allanwils)'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as http_error:
-        if response.status_code == 404:
-            return 0
-        else:
-            print(f"HTTP error occurred: {http_error}")
-            return 0
-    except requests.exceptions.RequestException as error:
-        print(f"An error occurred: {error}")
-        return 0
 
-    data = response.json()
-    if 'data' not in data or 'subscribers' not in data['data']:
-        print(f"Invalid response data from Reddit API for subreddit {subreddit}.")
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:v1.0.0 (by /u/Masiga)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
 
-    return data['data']['subscribers']
