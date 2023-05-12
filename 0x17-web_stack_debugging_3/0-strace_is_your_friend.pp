f18e0d0455f11a354945a32573dbe2d0e45a610f
Allan Masiga <zionalehebrew@gmail.com>
2023-05-12 12:10:16 +0300

    apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,12 @@
 # Fix Apache 500 error
-exec { 'fix-apache':
-  command => '/usr/local/bin/fix-apache',
-  onlyif  => 'test $(curl -s -o /dev/null -w "%{http_code}" http://localhost) = 200',
-}
+class apache {
+  exec { 'fix-apache':
+    command => '/usr/local/bin/fix-apache',
+    onlyif  => 'test $(curl -s -o /dev/null -w "%{http_code}" http://localhost) = 200',
+  }
 
-service { 'apache2':
-  ensure => running,
+  service { 'apache2':
+    ensure => running,
+  }
 }
+include apache
