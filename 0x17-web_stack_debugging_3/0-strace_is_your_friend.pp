c7c13ce90790ed642fd284075245fc12e7c16af3
Allanwils <zionalehebrew@gmail>
2023-05-21 12:49:34 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 exec { 'fix-wordpress':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp_test.php",
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp_test.php",
   path    => '/usr/local/bin/:/bin/',
+  returns => '0',
 }
