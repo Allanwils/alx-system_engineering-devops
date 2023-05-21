390b111c665040f7c2d8cadf69ce0be0a1d4b8c1
Allanwils <zionalehebrew@gmail>
2023-05-21 12:45:10 +0300

    0-strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,4 @@
-# Corrects bad `phpp` extensions to `php` in wp file `wp_test.php`.
-
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp_test.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => "sed -i 's/phpp/php/' /var/www/html/wp_test.php",
+  path    => '/usr/local/bin/:/bin/',
 }
