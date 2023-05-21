c4ab50f2d46d6722cec9e4de82ec41de615b1fce
Allanwils <zionalehebrew@gmail>
2023-05-21 08:54:16 +0300

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,72 +1,48 @@
-# File: web_stack_debugging.pp
-# Description: Puppet manifest for web stack debugging task
-
-# Install ApacheBench
-package { 'apache2-utils':
+# Install Nginx package
+package { 'nginx':
   ensure => installed,
 }
 
-# Configure Nginx
-class web_stack_debugging::nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('nginx/nginx.conf.erb'),
-    notify  => Service['nginx'],
-  }
-
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
+# Configure Nginx server settings
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  content => "# Nginx Configuration File
+worker_processes auto;
+events {
+  worker_connections 1024;
+}
+http {
+  client_max_body_size 10M;
+  keepalive_timeout 65;
+  sendfile on;
+  server {
+    listen 80;
+    server_name localhost;
+    access_log /var/log/nginx/access.log;
+    error_log /var/log/nginx/error.log;
+    location / {
+      proxy_pass http://backend;
+    }
   }
 }
+",
+}
 
-# Perform benchmarking using ApacheBench
+# Create logging directory
+file { '/var/log/nginx':
+  ensure => directory,
+}
+
+# Restart Nginx service
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/nginx.conf'],
+}
+
+# Run ApacheBench to simulate HTTP requests
 exec { 'benchmark':
-  command     => 'ab -n 1000 -c 100 http://localhost/',
-  logoutput   => true,
-  refreshonly => true,
-  subscribe   => Service['nginx'],
+  command => 'ab -n 2000 -c 100 http://localhost/',
+  path    => '/usr/bin',
+  require => Service['nginx'],
 }
-
-# Fix the web stack based on the benchmark results
-class web_stack_debugging::fix_web_stack {
-  # Analyze logs
-  file { '/var/log/nginx/access.log':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-  }
-
-  file { '/var/log/nginx/error.log':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-  }
-
-  # Implement fixes based on analysis
-  # Example fixes:
-  # - Adjust Nginx configuration settings
-  # - Optimize server resources
-  # - Fine-tune request handling parameters
-
-  # Restart Nginx after applying fixes
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => [Package['nginx'], File['/etc/nginx/nginx.conf']],
-  }
-}
-
-# Apply the manifest
-class { 'web_stack_debugging::nginx': }
-class { 'web_stack_debugging::fix_web_stack': }
